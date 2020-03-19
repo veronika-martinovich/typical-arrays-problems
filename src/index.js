45b4ec16fec8b344169c341049fe476d3add33b7
Veronika Martinovich <veranika.mart@gmail.com>
2020-03-19 18:22:11 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array == "undefined" || array.length < 1) return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array == "undefined" || array.length < 1) return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array == "undefined" || array.length < 1) return 0;
+  return array.reduce((sum, current) => sum + current)/array.length;
 }
